a64c318b2024e6fbbd7149b249e770e035ea3510
My-Cosmos <sangram1alt2acc3io@gmail.com>
2024-12-14 19:40:05 +0530

    Production Level **routes** Creation and Configuration setUp using **express.Router()** method for Creating and Exporting routes to app.js

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
+const userRouter = require('./routes/user.routes');
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req,res)=>{
-  res.render('index');
-})
+/**
+ * ~ In production level application we doesn't make/Create any route in 'app.js' file.
+ *   ~~ we only configure routes in 'app.js' file.
+ *   ~~ and make/Create routes in respective './routes' folder using **express.Router()** method.
+ *   ~~ and export the routes also using **express.Router()** method.
+ */
+app.use('/user', userRouter);
+
 
 app.listen(3000, ()=>{
   console.log(`Server is running on port 3000`);
