97c2146974642cc54f553f8cb96b49bf56a6c07e
My-Cosmos <sangram1alt2acc3io@gmail.com>
2024-12-14 22:19:44 +0530

    Put some Validation in registration form using express-validator npm package

--- routes/authRoutes/auth.routes.js
+++ routes/authRoutes/auth.routes.js
@@ -1,13 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
-router.get('/register',(req,res)=>{
-  res.render('authPages/register');
+router.get("/register", (req, res) => {
+  res.render("authPages/register");
 });
 
-router.post('/register', (req,res)=>{
-  console.log(req.body);
-  res.send("Got the Users");
-})
+router.post(
+  "/register",
+  body("email").trim().isEmail(),
+  body("password").trim().isLength({ min: 1 }),
+  body("username").trim().isLength({ min: 1 }),
+  (req, res) => {
+    console.log(req.body);
+    // ~ express-validator npm package method this is
+    const errors = validationResult(req);
+    // console.log(errors.errors);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+        message: "Invalid Data",
+      });
+    }
+
+    res.send("User successfully registered");
+  }
+);
 
 module.exports = router;
